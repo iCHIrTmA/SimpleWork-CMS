8874af6624406903a015ad7b48e55f5106428503
Janroe Cordero <jcdelloro@up.edu.ph>
2020-10-14 13:53:25 +0800

    Made create page

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -16,4 +16,20 @@ class ArticlesController extends Controller
     	$article = Article::find($id);
     	return view('articles.show', ['article' => $article]);
     }
+
+    public function create(){
+    	return view('articles.create');
+    }
+
+    public function store(){
+    	$article = new Article;
+    	
+    	$article->title = request('title');
+    	$article->excerpt = request('excerpt');
+    	$article->body = request('body');
+
+    	$article->save();
+
+    	return redirect('/articles');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,7 @@ Route::get('/about', function () {
     ]); 
 });
 
-Route::get('/articles/{article}', 'App\Http\Controllers\ArticlesController@show');
 Route::get('/articles', 'App\Http\Controllers\ArticlesController@index');
+Route::post('/articles', 'App\Http\Controllers\ArticlesController@store');
+Route::get('/articles/create', 'App\Http\Controllers\ArticlesController@create');
+Route::get('/articles/{article}', 'App\Http\Controllers\ArticlesController@show');
