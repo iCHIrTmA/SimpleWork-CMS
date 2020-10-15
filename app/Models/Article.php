aa3732a9dc4bb110f0c82ce6d00782840783a8f6
Janroe Cordero <jcdelloro@up.edu.ph>
2020-10-15 16:05:30 +0800

    Created Tags model and assigned tags to articles

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -18,5 +19,9 @@ class Article extends Model
 
     public function author(){
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
     }
 }
